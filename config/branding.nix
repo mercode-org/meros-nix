cd2998ffaa79704e82ef7162604da5a97e01c8de
Maciej Krüger <mkg20001@gmail.com>
2019-12-28 21:31:45 +0100

    feat: add branding

--- config/branding.nix
+++ config/branding.nix
@@ -5,6 +5,25 @@ with lib;
 {
   imports = [];
 
+  # NOTE: this file shouldn't enable anything, just set the branding attributes
+
+  services.xserver.displayManager.lightdm = {
+    background = "${pkgs.meros-backgrounds}/share/wallpapers/1.jpg";
+    greeters.gtk = {
+      iconTheme = {
+        package = pkgs.papirus-mer;
+        name = "papirus-mer";
+      };
+
+      extraConfig = ''
+        theme-name=mercode-jbrawn
+        xft-antialias=true
+        xft-hintstyle=hintfull
+        xft-rgba=rgb
+      '';
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     meros-backgrounds
     mercode-bazik
