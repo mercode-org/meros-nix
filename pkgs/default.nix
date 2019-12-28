bed1cfaf0757f3a8a85b50c61194fd042d46f14d
Maciej Krüger <mkg20001@gmail.com>
2019-12-28 22:05:01 +0100

    feat: initial packaging for merculator

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,17 @@
 let
   pkgs = import <nixpkgs> { };
+
+  nixNodePackage = builtins.fetchGit {
+    url = "https://github.com/mkg20001/nix-node-package";
+    rev = "898134fddca2e42a4ecdd6e386aac2869256f6d0";
+  };
+  mkNode = import "${nixNodePackage}/nix/default.nix" pkgs;
+
 in
 {
+  merculator = pkgs.callPackage ./merculator {
+    inherit mkNode;
+  };
   mercode-bazik = pkgs.callPackage ./mercode-bazik { };
   mercode-jbrawn = pkgs.callPackage ./mercode-jbrawn { };
   meros-backgrounds = pkgs.callPackage ./mer-os-backgrounds { };
