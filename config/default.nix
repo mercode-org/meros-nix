c33dad559250c4be8ceb598b5ba1a595ed37745d
Maciej Krüger <mkg20001@gmail.com>
2020-02-02 18:27:22 +0100

    feat: add cachix cache

--- config/default.nix
+++ config/default.nix
@@ -11,4 +11,13 @@ with lib;
   nixpkgs.overlays = [
     (import ../pkgs/overlay.nix)
   ];
+
+  nix = {
+    binaryCaches = [
+      "https://meros.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "meros.cachix.org-1:Zp80aqT/HTZgS8FybS7UpDv8IPo2jsbCluiNQ1PbouQ="
+    ];
+  };
 }
