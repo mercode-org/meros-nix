acb6f17aace2e73d7d737a8d0c0c9e848d3522e6
Maciej Krüger <mkg20001@gmail.com>
2020-02-10 02:14:42 +0100

    fix: quick-install

--- config/profiles/quick-install-vm.sh
+++ config/profiles/quick-install-vm.sh
@@ -6,4 +6,10 @@ yes | parted -s -a optimal /dev/sda mklabel msdos -- mkpart primary ext4 1 -1s
 mount /dev/sda1 /mnt
 conf-tool init --seed /etc/conf-tool-seed.json --template meros --root /mnt
 conf-tool add-user vmuser --root /mnt
+echo '{ config, lib, pkgs, ... }:
+
+{
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+}' > /mnt/etc/nixos/bootloader.nix
 nixos-install
