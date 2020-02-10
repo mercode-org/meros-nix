93a6cb9eb525ba7e989fdf4840537bb1b45da8c1
Maciej Krüger <mkg20001@gmail.com>
2020-02-10 23:45:38 +0100

    feat: add printing config

--- config/base/services.nix
+++ config/base/services.nix
@@ -43,6 +43,44 @@ makeDefault {
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  services.system-config-printer.enable = true;
+  environment.systemPackages = with pkgs; [ system-config-printer ];
+
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+
+    drivers = with pkgs; [
+      gutenprint
+      gutenprintBin
+      cups-googlecloudprint
+    ];
+
+    # this enables avahi+browsed autodiscover
+    browsing = true;
+    defaultShared = true;
+    extraConf = ''
+BrowseDNSSDSubTypes _cups,_print
+BrowseLocalProtocols all
+BrowseRemoteProtocols all
+CreateIPPPrinterQueues All
+    '';
+    browsedConf = ''
+BrowseDNSSDSubTypes _cups,_print
+BrowseLocalProtocols all
+BrowseRemoteProtocols all
+CreateIPPPrinterQueues All
+
+BrowseProtocols all
+    '';
+  };
+
+  # Cups network printing
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
   # And sane to scan
   hardware.sane.enable = true;
 }
