4f875470174711a692ebede044cf130d311e7497
Maciej Krüger <mkg20001@gmail.com>
2020-02-11 19:02:17 +0100

    feat: re-enable iso compilatioon

--- ci.nix
+++ ci.nix
@@ -2,7 +2,7 @@ let
   merOS = import ./release.nix;
 in
 {
-  # inherit (merOS) isoAll; # temporarily disabled because of CI problems
+  inherit (merOS) isoAll; # temporarily disabled because of CI problems
   cinnamonVm = merOS.cinnamon.vm;
   lxdeVm = merOS.lxde.vm;
   mateVm = merOS.mate.vm;
