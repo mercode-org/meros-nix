19de782c8dc3e703c6cb6ae6673830315c861f4e
Maciej Krüger <mkg20001@gmail.com>
2020-03-01 15:34:38 +0100

    fix: scopes

--- ci.nix
+++ ci.nix
@@ -1,16 +1,22 @@
 let
   merOS = import ./release.nix;
-  lib = merOS.nixpkgs.lib;
+
+  nixpkgs = import merOS.nixpkgs {};
+  pkgs = nixpkgs.pkgs;
+  lib = nixpkgs.lib;
+
+  makeScope = el: lib.makeScope pkgs.newScope (self: el);
 in
 {
   # inherit (merOS) isoAll; # disabled since using a lot of resources for nothing, mostly
-  vm = lib.newScope {
+
+  vm = makeScope {
     cinnamonVm = merOS.cinnamon.vm;
     lxdeVm = merOS.lxde.vm;
     mateVm = merOS.mate.vm;
     xfceVm = merOS.xfce.vm;
   };
 
-  tests = lib.newScope (merOS.tests {});
-  pkgs = lib.newScope (merOS.pkgs);
+  tests = makeScope (merOS.tests {});
+  pkgs = makeScope (merOS.pkgs);
 }
